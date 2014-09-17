34010d48dec9852e6f42f695ebf870a40bcd9796
MinixLi <MinixLi1986@gmail.com>
2014-09-17 19:28:13 +0800

    add comments to make code more maintainable

--- server/util/oauthUtil.js
+++ server/util/oauthUtil.js
@@ -1,13 +1,22 @@
-/*
+/**
  * oauthUtil
+ *
+ * @see http://mp.weixin.qq.com/wiki/index.php?title=网页授权获取用户基本信息
  */
 
 var request = require('request');
 var utils = require('./utils');
 
-module.exports.getAccessToken = getAccessToken;
+var oauthUtil = module.exports = {};
 
-function getAccessToken(app, code, cb) {
+/**
+ * Get access token by code
+ *
+ * @param {Object} app
+ * @param {String} code
+ * @param {Function} cb
+ */
+oauthUtil.getAccessToken = function(app, code, cb) {
   var settings = app.get('yuanhe_config');
   var appid = settings.appid;
   var secret = settings.secret;
@@ -20,7 +29,40 @@ function getAccessToken(app, code, cb) {
   });
 }
 
-function composeGetAccessTokenUrl(appid, secret, code) {
+/**
+ * Pull user info by access_token and openid
+ *
+ * @param {Object} app
+ * @param {String} access_token
+ * @param {String} openid
+ */
+oauthUtil.pullUserInfo = function(app, access_token, openid) {
+
+}
+
+/**
+ * Compose request url to get access_token
+ *
+ * @param {String} appid
+ * @param {String} secret
+ * @param {String} code
+ *
+ * @return {String}
+ */
+var composeGetAccessTokenUrl = function(appid, secret, code) {
   return 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='
     + appid + '&secret=' + secret + '&code=' + code + '&grant_type=authorization_code';
+}
+
+/**
+ * Compose request url to pull user info
+ *
+ * @param {String} access_token
+ * @param {String} openid
+ * @param {String} lang
+ *
+ * @return {String}
+ */
+var composePullUserInfo = function(access_token, openid, lang) {
+
 }
