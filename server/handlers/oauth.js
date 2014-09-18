b03e0c42345b53528501a911c4aa81ff2c9a35c8
MinixLi <MinixLi1986@gmail.com>
2014-09-18 16:27:32 +0800

    pull user info by access_token and openid

--- server/handlers/oauth.js
+++ server/handlers/oauth.js
@@ -3,11 +3,12 @@
  */
 
 var oauthUtil = require('../util/oauthUtil');
+var utils = require('../util/utils');
 
 exports.response = function(req, res) {
   var code = req.query.code;
 
-  oauthUtil.getAccessToken(code, function(err, resp, body) {
+  oauthUtil.getUserInfo(req, function(err, resp, body) {
     console.log(body);
   });
-}
+};
