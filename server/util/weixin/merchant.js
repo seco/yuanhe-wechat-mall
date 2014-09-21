e11444ee7cd422344842fe4197c428991e112779
MinixLi <MinixLi1986@gmail.com>
2014-09-22 01:57:44 +0800

    get order info by order id

--- server/util/weixin/merchant.js
+++ server/util/weixin/merchant.js
@@ -53,8 +53,64 @@ exp.getProductsByStatus = function(status, cb) {
 };
 
 /**
+ *  Get order info by id
+ *
+ *  @param {String} order_id
+ *  @param {Function} cb
+ *
+ *  @public
+ */
+exp.getOrderInfoById = function(order_id, cb) {
+  commonUtil.getAccessToken(function(err, access_token) {
+    if (err) {
+      utils.invokeCallback(cb, err);
+      return;
+    }
+
+    request({
+      url: composeGetOrderInfoByIdUrl(access_token),
+      method: 'POST',
+      body: JSON.stringify({
+        "order_id": order_id
+      })
+    }, function(err, resp, body) {
+      if (err) {
+        utils.invokeCallback(cb, err);
+        return;
+      }
+      try {
+        data = JSON.parse(body);
+      } catch(e) {
+        utils.invokeCallback(cb, err);
+        return;
+      }
+      if (data.errcode !== 0) {
+        utils.invokeCallback(cb, new Error(data.errmsg));
+        return;
+      }
+      utils.invokeCallback(cb, null, data.order);
+    });
+  });
+};
+
+/**
  * Compose request url to get products by status
+ *
+ * @param {String} access_token
+ *
+ * @private
  */
 var composeGetProductsByStatusUrl = function(access_token) {
   return 'https://api.weixin.qq.com/merchant/getbystatus?access_token=' + access_token;
+};
+
+/**
+ * Compose request url to get order info by id
+ *
+ * @param {String} access_token
+ *
+ * @private
+ */
+var composeGetOrderInfoByIdUrl = function(access_token) {
+  return 'https://api.weixin.qq.com/merchant/order/getbyid?access_token=' + access_token;
 };
