8c6e16e03e1007c95b82d7dcfd5a65ba532f9487
sicongtang <bobby3239@gmail.com>
2014-09-25 23:44:30 +0800

    update schema and dbops

--- server/dbops.js
+++ server/dbops.js
@@ -11,7 +11,7 @@ var initStores = function() {
       store_name: 'gitcafe store',
       store_type: 'virtual',
       store_address: 'pudongdadao 111hao',
-      telnum: 10086,
+      telnum: '10086',
       contact_name: 'bobby',
       unfollow: false,
       openid: '12345',
@@ -23,3 +23,86 @@ var initStores = function() {
     db.stores.insert(store);
   }
 };
+
+/**
+ * generate scene_id
+ *
+ */
+function getNextSequence(name) {
+  var ret = db.counters.findAndModify({
+    query: {_id: name},
+    update: {$inc: {seq: 1}},
+    new: true
+  });
+
+  return ret.seq;
+}
+
+
+
+/**
+ * create member(openid/productid/storeid/viewed_at)
+ * check view event
+ * follow store(then add following_store_id)
+ *
+ */
+db.members.insert({
+  openid: '',
+  unfollow: true,
+  created_at: new Date()
+});
+db.viewevents.insert({
+  member_id: ObjectId(''),
+  product_id: ObjectId(''),
+  store_id: ObjectId(''),
+  viewed_at: new Date()
+});
+
+db.viewevents.find({member_openid: ''}).sort(viewed_at: -1).limit(1);//return store_id that member visited recently
+db.members.update({_id: ObjectId('')},
+{
+  $set: {
+    unfollow: false,
+    following_store_id: ObjectId(''),
+    following_at: new Date(),
+    member_at: new Date()
+  }
+});
+
+
+
+/**
+ * get order request, sending to weixin
+ * callback and receive order detail
+ * find member(donot care whether he follow yuanhe or not) latest view_events, check store/product id
+ * create order
+ *
+ * syn weixin_order periodically, then update order: sales/member store and commission
+ *
+ */
+
+db.viewevents.find({member_openid: ''}).sort(viewed_at: -1).limit(1);//return store_id that member visited recently
+db.members.find({member_openid: ''});//return member following_store_id
+db.orders.insert({
+  order_status: 'create',
+  weixin_order_id: '',
+  weixin_order_detail: {},
+  sales_store:{
+    id: ObjectId(),
+    store_name: ''
+  },
+  member_store: {
+    id: ObjectId(),
+    store_name: ''
+  },
+  payment: 19.98,
+  product: {
+    id: ObjectId(),
+    price: 9.99,
+    quantity: 2
+  },
+  created_at: new Date()
+});
+
+db.orders.findOne({weixin_order_id: ''});
+db.orders.save({});
--- server/schema.js
+++ server/schema.js
@@ -5,19 +5,29 @@ var store = {
   store_name: 'gitcafe store',
   store_type: 'virtual',
   store_address: 'pudongdadao 111hao',
-  telnum: 10086,
+  telnum: '10086',
   contact_name: 'bobby',
   unfollow: false,
   openid: '12345',
+  scene_id: 100000,
   created_at: new Date()
+};
+//db.counters
+var counter = {
+  _id: 'store_id',
+  seq: 0
 };
 //db.products
 var product = {
-  _id: 77
+  _id: 77,
+  promotion_url: 'http://www.taobao.com'
 };
 //db.orders
 var order = {
   _id: 1,
+  order_status: '',
+  weixin_order_id: '',
+  weixin_order_detail: {},
   sales_store: {
     id: ObjectId('542270a9f13de9f92dc68d92'),
     store_name: '',
@@ -29,10 +39,10 @@ var order = {
     commission: 5.00
   },
   member: {
-    id: 88,
+    id: ObjectId(''),
     member_name: ''
   },
-  payment: 10.00,
+  payment: 9.99,
   product: {
     id: 1,
     price: 10.00,
@@ -42,16 +52,20 @@ var order = {
 };
 //db.members
 var member = {
-  _id: 88,
+  _id: ObjectId(''),
   openid: '',
-  unfollow: false,
-  store: {
-    id: ObjectId('542270a9f13de9f92dc68d92'),
-    store_name: ''
-  },
-  products: [{
-    id: 77,
-    viewed_at: new Date()
-  }],
-  created_at: new Date()
+  unfollow: true,
+  created_at: new Date(),
+  member_at: new Date(),
+  following_store_id: ObjectId('542270a9f13de9f92dc68d92'),
+  following_at: new Date()
+};
+//db.viewevents
+var viewevent = {
+  _id: ObjectId(''),
+  member_id: ObjectId(''),
+  member_openid: '',
+  product_id: ObjectId(''),
+  store_id: ObjectId('542270a9f13de9f92dc68d92'),
+  viewed_at: new Date()
 };
