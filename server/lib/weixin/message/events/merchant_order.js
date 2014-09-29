8fb6715d0abef1cbdcca1b176e1e8dbca005d075
MinixLi <MinixLi1986@gmail.com>
2014-09-29 12:28:00 +0800

    merchant_order event handler

---
+++ server/lib/weixin/message/events/merchant_order.js
@@ -0,0 +1,215 @@
+/**
+ * Merchant order event message handler
+ *
+ * @author Minix Li
+ */
+
+var async = require('async');
+var db = require('../../../../app').get('db');
+var dbProxy = require('../../../../app').get('dbProxy');
+var merchant = require('../../merchant');
+var utils = require('../../../util/utils');
+
+var MsgHandler = function() {};
+
+MsgHandler.prototype.name = 'merchant_order';
+
+/**
+ * message handler
+ *
+ * @public
+ */
+MsgHandler.prototype.handle = function(req, res, msg, cb) {
+  if (!msgIsValid(msg)) {
+    utils.invokeCallback(cb, new Error('invalid message'));
+    return;
+  }
+
+  // Fetch order info from weixin and insert it into database.
+  createOrder(order_id, function(err, result) {
+    if (err) {
+      utils.invokeCallback(cb, err);
+      return;
+    }
+
+    var order_id = result;
+
+    // Get the newest member event in the past 30 days
+    getMemberEvent(openid, function(err, event) {
+      if (err) {
+        utils.invokeCallback(cb, err);
+        return;
+      }
+
+      var sales_store_id, member_store_id;
+
+      if (event) {
+        var sales_store_id = event.store_id;
+
+        // get member by openid
+        getMemberByOpenId(openid, function(err, member) {
+          if (member.following_store_id) {
+            member_store_id = member.following_store_id;
+          } else {
+            member_store_id = sales_store_id;
+          }
+
+          // update order info
+          updateOrderInfo(order_id, sales_store_id, member_store_id, function(err, result) {
+            if (err) {
+              utils.invokeCallback(cb, err);
+              return;
+            }
+            utils.invokeCallback(cb, null);
+          });
+        });
+      } else {
+
+      }
+    });
+  });
+};
+
+/**
+ * Fetch order info from weixin and insert it into database.
+ *
+ * @param {String} order_id
+ *
+ * @private
+ */
+var createOrder = function(order_id, cb) {
+  async.auto({
+    order_info: function(cb) {
+      merchant.getOrderInfoById(order_id, cb);
+    },
+    collection: function(cb) {
+      dbProxy.collection("orders", cb);
+    },
+    create_order: ['order_info', 'collection', function(cb, results) {
+      var info = results.order_info;
+      var collection = results.collection;
+
+      collection.insert({ "weixin_order_info": order_info }, cb);
+    }],
+  }, function(err, result) {
+    if (err) {
+      utils.invokeCallback(cb, err);
+      return;
+    }
+    utils.invokeCallback(cb, null, result);
+  });
+};
+
+/**
+ * Get the newest member event in the past 30 days
+ *
+ * @param {String} openid
+ *
+ * @private
+ */
+var getMemberEvent = function(openid, cb) {
+  async.waterfall([
+    function(cb) {
+      dbProxy.collection("member_events", cb);
+    },
+    function(collection, cb) {
+      collection.find(
+        { "openid": openid },
+        { "limit": 1, "sort": { "time_created": -1 } },
+        cb
+      );
+    }
+  ], function(err, event) {
+    if (err) {
+      utils.invokeCallback(cb, err);
+      return;
+    }
+
+    if (event) {
+      var time_created = event.time_created;
+      if (time_created) {
+        utils.invokeCallback(cb, null, event);
+        return;
+      }
+    }
+    utils.invokeCallback(cb, null, null);
+  });
+};
+
+/**
+ * Get member by openid
+ *
+ * @param {String} openid
+ *
+ * @private
+ */
+var getMemberByOpenId = function(openid, cb) {
+  async.waterfall([
+    function(cb) {
+      dbProxy.collection("members", cb);
+    },
+    function(collection, cb) {
+      collection.find(
+        { "openid": openid },
+        cb
+      );
+    }
+  ], function(err, member) {
+    if (err) {
+      utils.invokeCallback(cb, err);
+      return;
+    }
+    utils.invokeCallback(cb, null, member);
+  });
+};
+
+/**
+ * Update order info
+ *
+ * @param {String} order_id
+ * @param {String} sales_store_id
+ * @param {String} member_store_id
+ *
+ * @private
+ */
+var updateOrderInfo = function(order_id, sales_store_id, member_store_id) {
+  async.waterfall([
+    function(cb) {
+      dbProxy.collection("orders", cb);
+    },
+    function(collection, cb) {
+      collection.update(
+        { "_id": order_id },
+        { "$set": {
+          "sales_store": { "id": sales_store_id },
+          "member_store": { "id" member_store_id }
+        } },
+        cb
+      );
+    }
+  ], function(err, result) {
+    if (err) {
+      utils.invokeCallback(cb, err);
+      return;
+    }
+    utils.invokeCallback(cb, null, result);
+  });
+};
+
+/**
+ * Check whether a message is valid
+ *
+ * @private
+ *
+ * @return {Boolean}
+ */
+var msgIsValid = function(msg) {
+  return false;
+};
+
+/**
+ * Initialize a new merchant order event message handler
+ */
+module.exports = function() {
+  return new MsgHandler();
+};
