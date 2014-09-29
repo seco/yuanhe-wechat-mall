12c752ef606943362c7dc288e979d566f98af35d
MinixLi <MinixLi1986@gmail.com>
2014-09-29 19:41:27 +0800

    update both sales and member stores of order

--- server/classes/yuanheOrder.js
+++ server/classes/yuanheOrder.js
@@ -11,26 +11,20 @@
  * @author Minix Li
  */
 
+var async = require('async');
+var dbProxy = require('../app').dbProxy;
 var utils = require('../lib/util/utils');
 var YuanheEntity = require('./yuanheEntity');
 
 /**
- * @param {String} id
- * @param {Function} cb
+ * YuanheOrder constructor
  */
-YuanheOrder = YuanheEntity.extend(function(id, cb) {
+YuanheOrder = function() {
   this.name = 'orders';
   this.initializeAttributes();
+};
 
-  if (id) {
-    this.load(this.name, id, function(err) {
-      if (err) {
-        utils.invokeCallback(cb, err);
-        return;
-      }
-    });
-  }
-});
+YuanheEntity.extend(YuanheOrder);
 
 var pro = YuanheOrder.prototype;
 
@@ -46,6 +40,64 @@ pro.initializeAttributes = function() {
   this.attributes['member'] = {};
   this.attributes['sales_store'] = {};
   this.attributes['member_store'] = {};
+};
+
+/**
+ * Load attributes from the order collection into the object
+ *
+ * @param {String} order_id
+ * @param {Function} cb
+ *
+ * @public
+ */
+pro.load = function(order_id, cb) {
+  this.load(this.name, order_id, function(err) {
+    if (err) {
+      utils.invokeCallback(cb, err);
+      return;
+    }
+    utils.invokeCallback(cb, null);
+  });
+}
+
+/**
+ * Update both sales and member stores
+ *
+ * @param {String} sales_store_id
+ * @param {String} member_store_id
+ * @param {Function} cb
+ *
+ * @public
+ */
+pro.updateStores = function(sales_store_id, member_store_id, cb) {
+  var sales_store_value = { "id": sales_store_id };
+  var member_store_value = { "id": member_store_id };
+
+  async.waterfall([
+    function(cb) {
+      dbProxy.collection(this.name, cb);
+    },
+    function(collection, cb) {
+      collection.update(
+        { "_id": this.get('_id') },
+        { "$set": {
+          "sales_store": sales_store_value,
+          "member_store": member_store_value
+        } },
+        cb
+      );
+    }
+  ], function(err, result) {
+    if (err) {
+      utils.invokeCallback(cb, err);
+      return;
+    }
+
+    this.set('sales_store', sales_store_value);
+    this.set('member_store', member_store_value);
+
+    utils.invokeCallback(cb, null);
+  });
 };
 
 /**
