66c8b4001ae899ea98f836ce866aa871c57d3a59
MinixLi <MinixLi1986@gmail.com>
2014-09-30 16:30:39 +0800

    write a decision tree class for business scalability

---
+++ server/classes/decisionTree.js
@@ -0,0 +1,47 @@
+/**
+ * Decision tree
+ *
+ * @author Minix Li
+ */
+
+/**
+ * DecisionTree constructor
+ *
+ * @param {object} context
+ * @param {DecisionTreeNode} root
+ */
+var DecisionTree = function(context, root) {
+  this.context = context;
+  this.root = root;
+};
+
+/**
+ * Start decisions
+ *
+ * @param {Function} cb
+ */
+DecisionTree.prototype.startDecisions = function(cb) {
+
+};
+
+/**
+ * DecisionTreeNode constructor
+ */
+var DecisionTreeNode = function() {
+  this.childNodes = [];
+  this.beforeDecision = null;
+};
+
+/**
+ * Add child to this node
+ *
+ * @param {DecisionTreeNode} node
+ *
+ * @public
+ */
+DecisionTreeNode.prototype.addChild = function(node) {
+  this.childNodes.push(node);
+};
+
+// export DecisionTree
+module.exports = DecisionTree;
