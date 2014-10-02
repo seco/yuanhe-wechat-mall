f443169e628673ff880de91c4f4d9afd6cc65e06
MinixLi <MinixLi1986@gmail.com>
2014-10-03 01:22:08 +0800

    product handler

---
+++ server/handlers/product.js
@@ -0,0 +1,17 @@
+/**
+ * Product handler
+ *
+ * @author Minix Li
+ */
+
+var async = require('async');
+var dbProxy = require('../../app').get('dbProxy');
+var utils = require('../lib/util/utils');
+
+/**
+ * @param {Object} req
+ * @param {Object} res
+ */
+exports.show = function(req, res) {
+
+};
