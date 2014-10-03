64a57be309cf90f8fd551aeb24619b98fc83f5b6
sicongtang <bobby3239@gmail.com>
2014-10-04 01:02:05 +0800

    set all the dev js file to non-min, and enable r.js to js/css optimization

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -33,29 +33,29 @@ require.config({
   },
 
   paths: {
-    jquery: '../bower_components/jquery/dist/jquery.min',
+    jquery: '../bower_components/jquery/dist/jquery',
     jqueryext: 'vendor/jquery_ext',
-    jqueryui: '../bower_components/jquery-ui/ui/minified/jquery-ui.min',
-    jqueryslimscroll: '../bower_components/jquery-slimscroll/jquery.slimscroll.min',
+    jqueryui: '../bower_components/jquery-ui/ui/jquery-ui',
+    jqueryslimscroll: '../bower_components/jquery-slimscroll/jquery.slimscroll',
     jquerycusttreemenu: 'vendor/jquery_cust_treemenu',
-    jqueryicheck: '../bower_components/jquery-icheck/icheck.min',
-    jquerynoty: '../bower_components/noty/js/noty/packaged/jquery.noty.packaged.min',
+    jqueryicheck: '../bower_components/jquery-icheck/icheck',
+    jquerynoty: '../bower_components/noty/js/noty/packaged/jquery.noty.packaged',
     'bootstrap-daterangepicker': '../bower_components/bootstrap-daterangepicker/daterangepicker',
     //daterangepicker: '../js/plugins/daterangepicker/daterangepicker',
-    moment: '../bower_components/moment/min/moment.min',
+    moment: '../bower_components/moment/moment',
     backbone: '../bower_components/backbone/backbone',
-    'backbone-deep-model': '../bower_components/backbone-deep-model/distribution/deep-model.min',
+    'backbone-deep-model': '../bower_components/backbone-deep-model/distribution/deep-model',
     //'backbone-nested': '../bower_components/backbone-nested/backbone-nested.min',
-    'backbone.paginator': '../bower_components/backbone.paginator/lib/backbone.paginator.min',
+    'backbone.paginator': '../bower_components/backbone.paginator/lib/backbone.paginator',
     backgrid: 'vendor/backgrid',
     /* bower has issue that it downloads a stale version, therfore we download manually  */
-    backgridpaginator: 'vendor/backgrid-paginator.min',
-    backgridtextcell: 'vendor/backgrid-text-cell.min',
-    backgridselectall: 'vendor/backgrid-select-all.min',
-    underscore: '../bower_components/underscore/underscore-min',
+    backgridpaginator: 'vendor/backgrid-paginator',
+    backgridtextcell: 'vendor/backgrid-text-cell',
+    backgridselectall: 'vendor/backgrid-select-all',
+    underscore: '../bower_components/underscore/underscore',
 
     /* alias the bootstrap js lib */
-    bootstrap: '../bower_components/bootstrap/dist/js/bootstrap.min',
+    bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
 
     adminLTE: '../js/AdminLTE/app',
 
