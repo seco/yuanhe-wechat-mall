9edb6239972bc174c0e23b694ea2ee20d31a3d7b
MinixLi <MinixLi1986@gmail.com>
2014-10-03 12:47:33 +0800

    yuanheGlobalCounter.js

---
+++ server/classes/yuanheGlobalCounter.js
@@ -0,0 +1,46 @@
+/**
+ * YuanheGlobalCounter
+ *
+ * @author Minix Li
+ */
+
+var async = require('async');
+var dbProxy = require('../app').dbProxy;
+var utils = require('../lib/util/utils');
+
+// YuanheGlobalCounter constructor
+YuanheGlobalCounter = function() {};
+
+/**
+ * Put collection name into class properties
+ */
+YuanheGlobalCounter.col_name = 'global_counters';
+
+/**
+ * Yield scene id
+ *
+ * @param {Function} cb
+ */
+YuanheGlobalCounter.yieldSceneId = function(cb) {
+  var col_name = this.col_name;
+
+  async.waterfall([
+    function(cb) {
+      dbProxy.collection(col_name, cb);
+    },
+    function(collection, cb) {
+      collection.findAndModify({ '_id': 'scene_id' }, [], { $inc: { value: 1 } }, { new: true }, cb);
+    }
+  ], function(err, doc) {
+    if (err) {
+      utils.invokeCallback(cb, err);
+      return;
+    }
+    utils.invokeCallback(cb, null, doc.value);
+  });
+};
+
+/**
+ * export YuanheMember
+ */
+module.exports = YuanheGlobalCounter;
