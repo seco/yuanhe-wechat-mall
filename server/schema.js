4d70d9ac2b8d01f707d8e1bd8437cda5ca82aec3
MinixLi <MinixLi1986@gmail.com>
2014-10-05 19:35:08 +0800

    update schema.js

--- server/schema.js
+++ server/schema.js
@@ -1,111 +1,17 @@
-//db.stores
-var store = {
-  _id: ObjectId('542270a9f13de9f92dc68d92'),
-  alias: 'gitcafe',
-  store_name: 'gitcafe store',
-  store_type: 'virtual',
-  store_address: 'pudongdadao 111hao',
-  telnum: '10086',
-  contact_name: 'bobby',
-  unfollow: false,
-  openid: '12345',
-  scene_id: 100000,
-  created_at: new Date()
-};
-//db.counters
-var counter = {
-  _id: 'scene_id',
-  seq: 0
-};
-//db.products
-var product = {
-  _id: 77,
-  weixin_product_info: {
-    'product_base': {
-      "name": "testaddproduct",
-      "img": ["http://mmbiz.qpic.cn/mmbiz/4whpV1VZl2iccsvYbHvnphkyGtnvjD3ulEKogfsiaua49pvLfUS8Ym0GSYjViaLic0FD3vN0V8PILcibEGb2fPfEOmw/0"]
-    },
-    'product_id': 'pDF3iY6Kr_BV_CXaiYysoGqJhppQ'
-  },
-  redirect_url: 'http://www.taobao.com'
-};
-//db.orders
-var order = {
-  order_status: 'created',
-  weixin_order_id: '',
-  weixin_order_info: {},
-  stores: [{
-    id: ObjectId('5423072cf13de9f92dc68dae'),
-    store_name: 'this is sales store name',
-    store_type: 'sales_store',
-    commission: 15.00
-  }, {
-    id: ObjectId('5423072cf13de9f92dc68daf'),
-    store_name: 'this is member store name',
-    store_type: 'member_store',
-    commission: 5.00
-  }],
-  member: {
-    id: ObjectId('5425316ca5ee873861df8c65'),
-    member_name: 'this is member nane'
-  },
-  payment: 9.99,
-  product: {
-    id: 1,
-    price: 10.00,
-    quantity: 2
-  },
-  created_at: new Date()
-};
-
-//db.members
-var member = {
-  _id: ObjectId(''),
-  openid: '',
-  channel_store_id: ObjectId(''),
-  time_following: new Date(),
-  time_created: new Date(),
-  time_updated: new Date(),
-  status: 'following'
-};
-
-// db.member_events
-var memberEvent = {
-  _id: ObjectId(''),
-  type: '',
-  member_openid: '',
-  object_id: ObjectId(''),
-  annotation_id: ObjectId(''),
-  posted: new Date(),
-};
-
-//db.accounts
-var account = {
-  _id: ObjectId('542a1ef1a5ee873861df8c6c'),
-  act_name: 'yuanhe_sa',
-  act_displayname: '元合超级管理员',
-  act_password: 'yuanhe_201410',
-  role_name: 'superadmin',
-  created_at: new Date()
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
 // db.stores
 var store = {
   _id: ObjectId(''),
   openid: '',
   scene_id: 100000,
-  name: '',
-  alias: '',
-  type: '',
-  address: '',
+  store_name: '',
+  store_type: '',
+  store_address: '',
   telnum: '',
+  alias: '',
   contact_name: '',
-  time_created: new Date(),
-  time_updated: new Date(),
-  // could be 'following' or 'unfollow'
-  status: ''
+  created_at: new Date(),
+  updated_at: new Date(),
+  unfollow: false
 };
 
 // db.members
@@ -113,11 +19,25 @@ var member = {
   _id: ObjectId(''),
   openid: '',
   channel_store_id: ObjectId(''),
-  time_created: new Date(),
-  time_updated: new Date(),
+  created_at: new Date(),
+  updated_at: new Date(),
   following_at: new Date(),
-  // could be 'following' or 'unfollow'
-  status: ''
+  unfollow: false
+};
+
+// db.member_events
+var memberEvent = {
+  _id: ObjectId(''),
+  // could be 'view', 'subscribe', etc.
+  type: '',
+  // member openid
+  member_openid: '',
+  // be used as store open id if it's a view event
+  store_id: ObjectId(''),
+  // be used as product id if it's a view event
+  weixin_product_id : '',
+  created_at: new Date(),
+  updated_at: new Date(),
 };
 
 // db.products
@@ -133,22 +53,9 @@ var product = {
       'img': ['', '']
     }
   },
-  redirect_url: ''
-};
-
-// db.member_events
-var memberEvent = {
-  _id: ObjectId(''),
-  // could be 'view', 'subscribe', etc.
-  type: '',
-  // member openid
-  member_openid: '',
-  // be used as store open id if it's a view event
-  store_id: ObjectId(''),
-  // be used as product id if it's a view event
-  weixin_product_id : '',
-  time_created: new Date(),
-  time_updated: new Date()
+  redirect_url: '',
+  created_at: new Date(),
+  updated_at: new Date()
 };
 
 // db.orders
@@ -157,25 +64,39 @@ var order = {
   weixin_order_id: '',
   weixin_order_info: {},
   stores: [{
-    id: ObjectId(''),
-    name: '',
-    type: 'sales',
+    store_id: ObjectId(''),
+    store_name: '',
+    store_type: 'sales',
     commission: 0.00
   }, {
-    id: ObjectId(''),
-    name: '',
-    type: 'channel',
+    store_id: ObjectId(''),
+    store_name: '',
+    store_type: 'channel',
     commission: 0.00
   }],
-  time_created: new Date(),
-  time_updated: new Date(),
+  member: {
+    member_openid: '',
+    member_name: ''
+  },
+  created_at: new Date(),
+  updated_at: new Date(),
   // could be 'settled' or 'unsettled'
-  status: ''
+  state: ''
 };
 
-// db.global_counters
+// db.counters
 // scend id document used to generate QR code
-var global_counter = {
+var counter = {
   _id: 'scene_id',
   value: 0,
+};
+
+//db.accounts
+var account = {
+  _id: ObjectId('542a1ef1a5ee873861df8c6c'),
+  act_name: 'yuanhe_sa',
+  act_displayname: '元合超级管理员',
+  act_password: 'yuanhe_201410',
+  role_name: 'superadmin',
+  created_at: new Date()
 };
